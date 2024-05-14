02b92e9b8e48a78c74a110057e0ea957e2a0b938
fahad <fahadsws1234@gmail.com>
2024-05-14 14:54:04 +0530

    ErpGo Rest Api

---
+++ Router/router.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const loginUser = require('../Controller/Auth/Login');
+const leavetype = require('../Controller/AllGetAPi/LeaveType');
+const middelware = require('../Middelware/TokenMidelware');
+const leavesumbit = require('../Controller/AllPostApi/LeaveMange');
+const addcomplaine = require('../Controller/AllPostApi/AddComplaine');
+const allemploye = require('../Controller/AllGetAPi/AllEmplye');
+const awards = require('../Controller/AllGetAPi/Award');
+const promotion = require('../Controller/AllGetAPi/Promotion');
+const leaves = require('../Controller/AllGetAPi/Leaves');
+const complaine = require('../Controller/AllGetAPi/Complaines');
+const profileupdate = require('../Controller/AllPostApi/ProfileUpdate');
+const forgotpassword = require('../Controller/AllPostApi/ForgotPassword');
+const forgotpass = require('../Controller/AllPostApi/ForgotPass');
+const support = require('../Controller/AllGetAPi/Support');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.post('/login', loginUser);
+router.get('/leavetype',middelware, leavetype);
+router.post('/leavesumbit',middelware, leavesumbit);
+router.post('/addcomplaine',middelware, addcomplaine);
+router.get('/allemploye',middelware, allemploye);
+router.get(`/awards/:id`,middelware, awards);
+router.get(`/promotion/:id`,middelware, promotion);
+router.get(`/leaves/:id`,middelware, leaves);
+router.get(`/complaine/:id`,middelware, complaine);
+router.post('/profileupdate',middelware, profileupdate);
+router.post('/forgotpassword',middelware, forgotpassword);
+router.get('/forgotpass', forgotpass);
+router.get('/support', support);
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
